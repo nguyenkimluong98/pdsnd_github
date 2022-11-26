427a0f4019cae63aa48557d3f6755425f61684d5
nguyenkimluong98 <nguyenkimluong98@gmail.com>
2022-11-26 19:33:43 +0700

    refactor: add notification to console after application shutdown

--- bikeshare.py
+++ bikeshare.py
@@ -221,6 +221,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print('Application turning off...')
             break
 
 
